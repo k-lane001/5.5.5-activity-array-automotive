1d2aed06b5d8cd774884bca2c29776a3c8cff3cf
Kaylie Lane <k.lane5612@gmail.com>
2022-04-13 17:40:59 -0400

    Final

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,35 +1,47 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+import { Vehicle as VehicleModule } from './vehicle';
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-class a28Mercury extends v {
+// *** new Car class ***
+
+class Car extends v {
     constructor(maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService) {
-        super(make, model, year, color, speed, started);
-        this.maximumPassengers = 5;
-        this.passenger = 0;
-        this.numberOfWheels = 4;
-        this.maximumSpeed = 160;
-        this.fuel = 10;
-        this.scheduleService = 'false';
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = maximumPassengers;
+        this.passenger = passenger;
+        this.numberOfWheels = numberOfWheels;
+        this.maximumSpeed = maximumSpeed;
+        this.fuel = fuel;
+        this.scheduleService = scheduleService;
     }
     loadPassenger(num) {
-        if(this.passenger < this.maximumPassengers) {
-            availableRoom == 'true';
-            console.log('There is room for more passengers.')
-        } 
+        if(num + this.passenger <= this.maxPassengers) {
+                console.log('Load Passengers');
+        } else {
+            console.log('There is not enough room for all passengers.');
+        }
     }
     start() {
         if(this.fuel > 0) {
-            start == 'true';
+            start == true;
+            console.log('Start the engine');
+        } else {
+            start == false;
+            console.log('Fill the tank');
         }
     }
     scheduleService(mileage) {
         if(this.mileage > 30000) {
-            timeForMaintenance == 'true';
+            scheduleService == true;
             console.log('Time for a tune-up!')
+        } else {
+            scheduleService == false;
+            console.log('Remember to get a tune-up at 30,000 miles!')
         }
     }
 }
+// *** new Car object ***
+let A28MercurySedan = new Car ["Mecury", "Sedan", "1965", "color", "mileage", 5, 0, 4, 160, 10, false];
